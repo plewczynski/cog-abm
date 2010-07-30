4ff9e36de177c057ae9ebfe824fa2fb99d5bdb72
Konrad Kurdej <kk249297@students.mimuw.edu.pl>
2010-07-30 11:42:49 +0200

    Small update + very simple flu epidemic simulation

--- src/cog_abm/core/simulation.py
+++ src/cog_abm/core/simulation.py
@@ -1,6 +1,7 @@
 """
 Module providing flow control in simulation
 """
+import random
 
 class Simulation(object):
 	"""
@@ -10,9 +11,10 @@ class Simulation(object):
 	global_environment = None
 	environments = []
 	
-	def __init__(self,  graph = None,  interaction = None):
+	def __init__(self,  graph = None,  interaction = None,  agents = None):
 		self.graph = graph
 		self.interaction = interaction
+		self.agents = agents
 		#self.
 		
 	def run(self, iterations = 1000):
@@ -21,9 +23,28 @@ class Simulation(object):
 		
 		iterations
 		"""
+		n = len(self.agents)
 		for i in range(iterations):
-			#whatever
-			print i
+			if self.interaction.num_agents() == 2:
+				(a, b) = random.choice(self.graph.edges())
+				agents = [self.agents[a], self.agents[b]]
+			else :
+				pass
+			self.interaction.interact(agents)
+			
+			for i in range(n):
+				print "A["+str(i)+"]="+ self.agents[i].state.stan+ " |", 
+			
+			def counter(stan, w, a):
+				if a.state.stan == stan:
+					return w+1
+				else:
+					return w
+				
+			zar = reduce(lambda x, a:counter("ZA", x, a), self.agents,  0)
+			cho = reduce(lambda x, a:counter("CH", x, a), self.agents,  0)
+			zdr = reduce(lambda x, a:counter("ZD", x, a), self.agents,  0)
+			print "ZA:"+str(zar)+"  CH: "+str(cho)+"  ZD: "+str(zdr)
 			
 		
 		#TODO: should return some history
